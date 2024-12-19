7b1091684b1511ee6bb8d65d246cb8d0b50895ae
007anthony <anthony.pena@lernende.bbw.ch>
2024-12-19 09:55:03 +0100

    add user manager

---
+++ user-manager/src/test/java/ch/bbw/ap/shop/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.ap.shop.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
