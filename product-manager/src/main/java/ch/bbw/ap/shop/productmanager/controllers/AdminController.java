cea5dd663d1f7922470d324696921045f6e4c913
007anthony <anthony.pena@lernende.bbw.ch>
2025-01-09 08:55:16 +0100

    implement edit endpoint for product

--- product-manager/src/main/java/ch/bbw/ap/shop/productmanager/controllers/AdminController.java
+++ product-manager/src/main/java/ch/bbw/ap/shop/productmanager/controllers/AdminController.java
@@ -4,11 +4,10 @@ import ch.bbw.ap.shop.productmanager.mapper.ProductMapper;
 import ch.bbw.ap.shop.productmanager.models.Product;
 import ch.bbw.ap.shop.productmanager.models.ProductRequest;
 import ch.bbw.ap.shop.productmanager.services.ProductService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("/admin")
 public class AdminController {
@@ -22,6 +21,17 @@ public class AdminController {
     @PostMapping("/products")
     public Product createProduct(@RequestBody ProductRequest product) {
         return productService.createProduct(product);
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> editProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+
+        Product product = productService.editProduct(id, productRequest);
+
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
     }
 
 }
--- product-manager/src/main/java/ch/bbw/ap/shop/productmanager/mapper/ProductMapper.java
+++ product-manager/src/main/java/ch/bbw/ap/shop/productmanager/mapper/ProductMapper.java
@@ -8,4 +8,9 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     Product map(ProductRequest productRequest);
+
+    @Mapping(target = "id", source = "product.id")
+    @Mapping(target = "product", source = "productRequest.product")
+    @Mapping(target = "price", source = "productRequest.price")
+    Product merge(ProductRequest productRequest, Product product);
 }
