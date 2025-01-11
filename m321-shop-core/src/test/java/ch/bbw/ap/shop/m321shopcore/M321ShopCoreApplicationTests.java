156389d7bb9b9001cb806e744a65f0a31be163cd
007anthony <anthony.pena@lernende.bbw.ch>
2025-01-11 19:01:51 +0100

    create core module

---
+++ m321-shop-core/src/test/java/ch/bbw/ap/shop/m321shopcore/M321ShopCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.ap.shop.m321shopcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M321ShopCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
