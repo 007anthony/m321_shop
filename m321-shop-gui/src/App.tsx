d554866a1adfdd0e961d4c1ddbcec86a2fc53428
007anthony <anthony.pena@lernende.bbw.ch>
2025-01-18 18:15:42 +0100

    fix token retrieval

--- m321-shop-gui/src/App.tsx
+++ m321-shop-gui/src/App.tsx
@@ -7,40 +7,38 @@ import Signup from "./views/Signup/Signup";
 import UserService from "./services/UserService";
 import User from "./models/User";
 import {useSessionStorage} from "../hooks/SessionStoragehook";
+import ProductDetail from "./views/ProductDetail/ProductDetail";
 
 function App() {
 
     const [user, setUser] = useState<User>();
 
-    const [token, setToken] = useSessionStorage('token');
-
-
-
-    useEffect(() => {
-        setToken(sessionStorage.getItem("token"))
-    }, [])
+    const [token, setToken, removeToken] = useSessionStorage('token');
 
     useEffect(() => {
         if(token) {
             UserService.getUserDetails(token)
                 .then(user => setUser(user))
-                .catch(e => console.log(e))
+                .catch(e => removeToken())
         }
     }, [token]);
 
 
   return (
     <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-              {!user? (<>
-                    <Route path="/signup" element={<Signup/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                  </>
-                  ): ''}
-          </Routes>
-        </BrowserRouter>
+        <main>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/product/:id" element={<ProductDetail/>}/>
+                  {!user? (<>
+                        <Route path="/signup" element={<Signup/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                      </>
+                      ): ''}
+              </Routes>
+            </BrowserRouter>
+        </main>
     </>
   )
 }
